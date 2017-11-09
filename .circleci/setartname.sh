27ffe7db2d51779cc05804f022f8ef3f6dc19642
Diego Sampaio <chinello@gmail.com>
2017-11-09 15:13:55 -0200

    Build own docker image

--- .circleci/setartname.sh
+++ .circleci/setartname.sh
@@ -8,16 +8,20 @@ if [[ $CIRCLE_TAG =~ ^[0-9]+\.[0-9]+\.[0-9]+-rc\.[0-9]+ ]]; then
   SNAP_CHANNEL=candidate
   RC_RELEASE=candidate
   RC_VERSION=$CIRCLE_TAG
+  DOCKER_TAG=release-candidate
 elif [[ $CIRCLE_TAG =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
   SNAP_CHANNEL=stable
   RC_RELEASE=stable
   RC_VERSION=$CIRCLE_TAG
+  DOCKER_TAG=latest
 else
   SNAP_CHANNEL=edge
   RC_RELEASE=develop
   RC_VERSION=0.60.0-develop
+  DOCKER_TAG=develop
 fi
 
 export SNAP_CHANNEL
 export RC_RELEASE
 export RC_VERSION
+export DOCKER_TAG
